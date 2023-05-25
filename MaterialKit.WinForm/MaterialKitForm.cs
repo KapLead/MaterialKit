3ecbc06af1517b76fdc0a9d2be93514510194733
KAP <KAP@DESKTOP-DNB9NIU>
2023-05-25 15:09:01 +0300

    Добавьте файлы проекта.

---
+++ MaterialKit.WinForm/MaterialKitForm.cs
@@ -0,0 +1,99 @@
+﻿using System.Windows.Forms;
+
+namespace MaterialKit.WinForm
+{
+    public partial class MaterialKitForm : Form
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        public MaterialKitForm()
+        {
+            InitializeComponent();
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.buttonCloseForm = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 59);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.panel3);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(800, 26);
+            this.panel2.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.buttonCloseForm);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel3.Location = new System.Drawing.Point(673, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(127, 26);
+            this.panel3.TabIndex = 0;
+            // 
+            // buttonCloseForm
+            // 
+            this.buttonCloseForm.Dock = System.Windows.Forms.DockStyle.Right;
+            this.buttonCloseForm.FlatAppearance.BorderSize = 0;
+            this.buttonCloseForm.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonCloseForm.Location = new System.Drawing.Point(101, 0);
+            this.buttonCloseForm.Name = "buttonCloseForm";
+            this.buttonCloseForm.Size = new System.Drawing.Size(26, 26);
+            this.buttonCloseForm.TabIndex = 0;
+            this.buttonCloseForm.UseVisualStyleBackColor = true;
+            // 
+            // MaterialKitForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "MaterialKitForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MaterialKitForm";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Panel panel2;
+        private Panel panel3;
+        public Button buttonCloseForm;
+    }
+}
---
+++ MaterialKit.WinForm/Themes/ColorSheme.cs
@@ -0,0 +1,27 @@
+﻿using System.Drawing;
+
+namespace MaterialKit.WinForm.Themes
+{
+    public class ColorSheme
+    {
+        /// <summary> цвет фона окна или контрола </summary>
+        public Color Background { get; private set; }
+
+        /// <summary> цвет фона активного поля элемента </summary>
+        public Color BackFields { get; private set; }
+  
+        /// <summary> акцентированный цвет фона  </summary>
+        public Color BackAccept { get; private set; }
+
+        /// <summary> цвет шрифта окна или контрола </summary>
+        public Color Foreground { get; private set; }
+
+        /// <summary> цвет шрифта активного контрола </summary>
+        public Color ForeFields { get; private set; }
+
+        /// <summary> акцентированный цвет шрифта  </summary>
+        public Color ForeAccept { get; private set; }
+
+
+    }
+}
